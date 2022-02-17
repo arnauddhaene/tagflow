423597e0654d11a3ce053083869a3b7e822889e7
Arnaud Dhaene <arnaud.dhaene@epfl.ch>
2022-02-16 18:33:35 -0800

    New: upload .dcm files using st.file_uploader

--- tagflow/home.py
+++ tagflow/home.py
@@ -3,26 +3,36 @@ from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pydicom
 
 import streamlit as st
 
+from .utils import load_dcm_series
 from .widgets.player import Player
 
 
-@st.cache
-def load_data():
-    npz = np.load('torch_track/sample_data/in_vivo_data.npz')
-    y1r = np.load('torch_track/sample_data/in_vivo_data_deformation.npz')['y1r']
-    imt, r0 = npz['imt'], npz['r0']
-    return imt, r0, y1r.swapaxes(0, 2)
+# @st.cache
+# def load_data():
+#     npz = np.load('torch_track/sample_data/in_vivo_data.npz')
+#     y1r = np.load('torch_track/sample_data/in_vivo_data_deformation.npz')['y1r']
+#     imt, r0 = npz['imt'], npz['r0']
+#     return imt, r0, y1r.swapaxes(0, 2)
 
 
 def write():
     
-    imt, r0, y1r = load_data()
+    # imt, r0, y1r = load_data()
     
-    st.session_state.image = imt
-    # st.session_state.points = y1r
-    # st.session_state.reference = r0
+    if st.session_state.image is None:
         
-    Player(st.session_state.image, st.session_state.points).display()
+        images = st.file_uploader('Upload series of DICOM files (in order)', type='dcm',
+                                  accept_multiple_files=True)
+        
+        if len(images) > 0:
+            res = map(lambda ds: (ds.InstanceNumber, ds.pixel_array), map(pydicom.dcmread, images))
+            
+            st.session_state.image = np.array(list(zip(*sorted(res, key=lambda item: item[0])))[1])
+                        
+            Player(st.session_state.image, st.session_state.points).display()
+    else:
+        Player(st.session_state.image, st.session_state.points).display()
